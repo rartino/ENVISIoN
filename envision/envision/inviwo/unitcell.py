Fix scaling factors.

envision/envision/inviwo/unitcell.py
@@ -54,8 +54,12 @@ def _cellnetwork(h5file, md=False, xpos=0, ypos=0):
     with h5py.File(h5file,"r") as h5:
         basis_matrix = np.array(h5["/basis"], dtype='d')
         strucMesh_basis_property = strucMesh.getPropertyByIdentifier('basis')
+        strucMesh_basis_property.maxValue = inviwopy.glm.mat3(1000000,1000000,1000000,1000000,1000000,1000000,1000000,1000000,1000000)
         strucMesh_basis_property.value = inviwopy.glm.mat3(basis_matrix[0,0],basis_matrix[0,1],basis_matrix[0,2],basis_matrix[1,0],basis_matrix[1,1],basis_matrix[1,2],basis_matrix[2,0],basis_matrix[2,1],basis_matrix[2,2])
         #inviwo.setPropertyValue(strucMesh+'.basis', tuple(map(tuple, basis_matrix)))
+        strucMesh_scaling_factor_property = strucMesh.getPropertyByIdentifier('scalingFactor')
+        strucMesh_scaling_factor_property.maxValue = h5['/scaling_factor'].value
+        strucMesh_scaling_factor_property.value = h5['/scaling_factor'].value
         strucMesh_timestep_property = strucMesh.getPropertyByIdentifier('timestep')
         strucMesh_timestep_property.value = 0
         strucMesh_timestep_property.minValue = 0
@@ -105,7 +109,7 @@ def _cellnetwork(h5file, md=False, xpos=0, ypos=0):
             #inviwo.setPropertyValue(coordReader+'.path', base_group + '/Atoms/'+key)
             strucMesh_radius_property = strucMesh.getPropertyByIdentifier('radius{0}'.format(i))
             #strucMesh_radius_property.maxValue = 3
-            strucMesh_radius_property.value = radius/10
+            strucMesh_radius_property.value = radius
             strucMesh_color_property = strucMesh.getPropertyByIdentifier('color{0}'.format(i))
             strucMesh_color_property.value = inviwopy.glm.vec4(color[0],color[1],color[2],color[3])
             '''

envision/envision/parser/vasp/unitcell.py
@@ -260,7 +260,7 @@ def unitcell(h5file, vasp_dir, elements=None):
                 _cartesian(f),
                 np.linalg.inv(basis)
             )
-            _write_basis(h5file, scaling_factor * basis)
+            _write_basis(h5file, basis)
             _write_scaling_factor(h5file, scaling_factor)
             _write_coordinates(
                 h5file,