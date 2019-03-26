596ab75d45e692f880aa064ea5aec546c0b61da0
ASD <asd@asd.com>
2019-03-26 18:05:47 +0100

    Created an outline for parsersystem of PKF and tested some functionalities.

--- envision/envision/parser/vasp/PKF.py
+++ envision/envision/parser/vasp/PKF.py
@@ -11,61 +11,57 @@ sys.path.append(os.path.abspath(PATH_TO_parser))
 
 import h5writer 
 
-#function called from script in inviwo 
-def paircorrelation(h5file = 0, vasp_dir = 0):
-	"""
-	Reads PCDAT-file and stores the data in an HDF5-file. 
 
-	Parameters 
-	___________
-	h5file : str 
+#The function script from inviwo will call with envision.parser.vasp.paircorrelation(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+print("This should work!!!")
+
+def paircorrelation(h5file, vasp_dir):
+
+	"""
+	The function is called from ENVISIoNs (alt.inviwo) environment to parse relevant information 		for visualization of the paircorrelation function.    
+
+	Parameters
+	__________
+	h5file: str
 		String that asserts which HDF5-file to write to. 
-	vasp_dir : str 
-		Path to directory containing volume file. 
 
-	Returns 
-	________ 
-	bool
-		True if PCDAT was parsed, false otherwise. 
-	
-	"""
-	fileclass = h5py.File("namba", "w") 
-	grp = fileclass.create_group("subgroup1")
-	grp2 = fileclass.create_group("subgroup2")
-
-	subgrp2 = grp2.create_group("subsubgroup2")
-	subgrp3 = subgrp2.create_group("subsubsubgroup2")
-
-	dset = grp.create_dataset("blabla", (100,), dtype='i')
-	dset2 = subgrp3.create_dataset("a special dataset", (100,), dtype='i')
-
-
-	#with h5py.File("mytestfile.hdf5", "w") as f:
-	#	dset = f.create_dataset("mydataset", (100,), dtype='i')
-	#	stringname = dset.name 
-	#	fileobject = f.name 
-
-	#print(str(20))
-	#print(stringname)
-	#print(fileobject)
-	if "/subgroup2/subsubgroup2/subsubsubgroup2/a special dataset" in fileclass: #if in notation only looks after groups directly after fileclass 
-		print("Yeas! kan lasa dataset")
-		#print(list(fileclass.keys())) #lista keys = groups till HDFfil
-		print(dset2.name)
-		return fileclass 
-	
-	print("koko")
-	print(grp2.name)
-	return fileclass
+	vasp_dir: str
+		Path to directory containing VASP-files, such as PCDAT.  
 
 
 	
-	#is the file already parsed? 
-	#if os.path.isfile(h5file): 
-	#	with h5py.File(h5file, 'r') as h5: 
-	#		if "" 
+	Return
+	_______
+	bool 
+		True if parsing is taking place, False otherwise.	
+
+	""" 
+
+	#Check if it's parsed, no need for parsing if it's already done. 
+"""	if os.path.isfile(h5file): 
+		with h5py.File(h5file, 'r') as h5: 
+			if '/PairCorrelationFunc' in h5: 
+				print('PCDAT already parsed. Skipping.')
+				return False
+
+	try: 
+		with open(os.path.join(vasp_dir, 'PCDAT'), 'r') as vasp_fileobj:
+			pcdat_data = _parse_pcdat(h5file, vasp_fileobj) #vasp_fileobj is a fileobject 
+			
+
+	except FileNotFoundError:
+		print("PCDAT file not in directory. Skipping.")
+		return False 
+
+	_write_pcdat(h5file, pcdat_data)
+	print('PCDAT data was parsed sucessfully.')
+	return True 		"""	 
 	
-fileobj= paircorrelation() 
-#print(list(fileobj.keys())) 
+
+#_parse_pcdat(h5file, vasp_fileobj)
+#_write_pcdat(h5file, pcdat_data) pcdat_data is a list for relevant atom n. 
+
+	
+
 
 
