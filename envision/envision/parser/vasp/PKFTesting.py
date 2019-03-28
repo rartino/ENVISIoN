60592e329288d407e27238ba3cc21ccae0185583
Linda Le <linda_le@c-10-253-225-184.clients.eduroam.liu.se>
2019-03-28 21:02:02 +0100

    _parse_pcdat(,) is done.

--- envision/envision/parser/vasp/PKFTesting.py
+++ envision/envision/parser/vasp/PKFTesting.py
@@ -5,8 +5,8 @@ import h5py
 import numpy as np
 
 # make module available
-#PATH_TO_HDF5 = os.path.expanduser("~/ENVISIoN/HDF5/demo.hdf5")
-#PATH_TO_VASP_CALC = os.path.expanduser("~/ENVISIoN/data")
+PATH_TO_HDF5 = os.path.expanduser("~/ENVISIoN/HDF5/demo.hdf5")
+PATH_TO_VASP_CALC = os.path.expanduser("~/ENVISIoN/data/VASP-files2019/Cu/1/10")
 PATH_TO_parser = os.path.expanduser("~/ENVISIoN/envision/envision/parser")
 sys.path.append(os.path.abspath(PATH_TO_parser))
 
@@ -26,12 +26,43 @@ def _parse_pcdat(h5file, vasp_file):
 
 #    Return
 #    ______
-#    A list of values, the average number of atoms type n given equally spaced distances from an average atom of type n.
+#   dict or False.
+#   Function returns False when parsing fails.
+#   A dictionary with the appearance {'element_type':[PKF_values]}. PKF_values are the average number of atoms, of specific element_type. If the system has elements 'Si', 'Au' and 'K', the dictionary will look like {'Si':float[x], 'Au':float[x], 'K':float[x]} where the float[x] is a list with the PKF values.
 
-#    Läs hur många atomslag som finns
     with open(vasp_file, "r") as vasp_fileobj:
         line = next(vasp_fileobj)
-        print(line)
+        line = next(vasp_fileobj)
+        line = next(vasp_fileobj) #Third line specify which elements are in the system
+        
+        # See if PCDAT has relevant information?
+        element_list = line.split()
+        if element_list[0]=="unknown" and element_list[1]=="system":
+            print("Parsing of PCDAT abort! File doesn't contain relevant information. Skipping.")
+            return False
+        else:
+            pcdat_data = {}
+            total_elements = len(element_list)
+            for i in range(total_elements):
+                pcdat_data[element_list[i]] = []
+                #To make a dictionary of the appearance {'Si':[], 'Au':[]...}
+            
+                
+            #fill the dictionary pcdat_data
+            for row_num, content in enumerate(vasp_fileobj, 3):
+                if row_num >= 12: #Values from PKF
+                    
+                    #takes the PKF value columnwise.
+                    #The first iteration for example gives
+                    #{'Si':[PKF_row1column1], 'Au':[PKF_row1column2], 'K':[PKF_row1column3]...}
+                    for column_num in range(total_elements):
+                        PKF_value = float(content.split()[column_num])
+                        #dict_key is example 'Si'
+                        dict_key = element_list[column_num]
+                        #Fill the pcdat_data dictionary
+                        pcdat_data[dict_key].append(PKF_value)
+
+        return pcdat_data
 
 
 # The function script from inviwo will call with envision.parser.vasp.paircorrelation(PATH_TO_HDF5, PATH_TO_VASP_CALC)
@@ -51,7 +82,12 @@ def paircorrelation(h5file, vasp_dir):
 #    ______
 #    Bool: True if parsed, False otherwise.
 
-    vasp_file = os.path.join(vasp_dir, "PCDAT")
-    _parse_pcdat(h5file, vasp_file)
+#See if parsing fails?
+#See if parsing is already done? Skipping.
+    vasp_file = os.path.join(vasp_dir, "PCDAT.dms")
+    dict = _parse_pcdat(h5file, vasp_file)
 
-#paircorrelation(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+
+paircorrelation(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+
+
