2ab54d3898608789461238a145430f7b09a1597b
Anthj975 <47103941+Anthj975@users.noreply.github.com>
2019-04-02 16:13:10 +0200

    Folder changed for GUI

---
+++ envision/envision/GUI/frameCharge.py
@@ -0,0 +1,21 @@
+import wx,sys,os
+from parameter_utils import *
+from VisFrame import *
+class ChargeFrame(wx.CollapsiblePane):
+    def __init__(self, *args, **kwargs,):
+        wx.CollapsiblePane.__init__(self,*args,**kwargs)
+        
+        chargePane = self.GetPane()
+        chargeSizer = wx.BoxSizer(wx.VERTICAL)
+        chargePane.SetSizer(chargeSizer)
+        self.button1=wx.Button(chargePane, label = str('X'))
+        chargeSizer.Add(self.button1,
+                     wx.GROW | wx.ALL, 2)
+        chargeSizer.Add(wx.Button(chargePane, label = str('Y')),
+                     wx.GROW | wx.ALL, 2)
+        chargePane.SetSizer(chargeSizer)
+
+        self.button1.Bind(wx.EVT_BUTTON, self.button_pressed_X)
+
+    def button_pressed_X(self,event):
+        charge_clear_tf()
