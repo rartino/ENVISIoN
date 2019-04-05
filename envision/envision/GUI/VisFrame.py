d25277e3df1a514f94103e2e378b8c7469a5744d
Anthj975 <47103941+Anthj975@users.noreply.github.com>
2019-04-05 15:59:54 +0200

    Added functionalty to textctrl

--- envision/envision/GUI/VisFrame.py
+++ envision/envision/GUI/VisFrame.py
@@ -1,3 +1,41 @@
+#
+#  ENVISIoN
+#
+#  Copyright (c) 2019 Anton Hjert
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##############################################################################################
+#
+#  Alterations to this file by Anton Hjert
+#
+#  To the extent possible under law, the person who associated CC0
+#  with the alterations to this file has waived all copyright and related
+#  or neighboring rights to the alterations made to this file.
+#
+#  You should have received a copy of the CC0 legalcode along with
+#  this work.  If not, see
+#  <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 """*****************************************************************************"""
 """This file sets up the visualization-section of the GUI, a collapsible pane.  """
 """The subsections of this pane is either items, such as boxes, or collapsible  """
@@ -13,9 +51,9 @@ from frameParchg import ParchgFrame
 from frameUnitcell import UnitcellFrame
 
 sys.path.insert(0, os.path.expanduser("C:/ENVISIoN/envision"))
-import envision
-import envision.inviwo
-import parameter_utils
+#import envision
+#import envision.inviwo
+#import parameter_utils
 PATH_TO_HDF5=os.path.expanduser("C:/Users/sille/Downloads/demo_charge.hdf5")
 
 class VisualizationFrame(wx.CollapsiblePane):
@@ -34,11 +72,11 @@ class VisualizationFrame(wx.CollapsiblePane):
         self.fileText = wx.StaticText(self.GetPane(),
                                     label="File to Visualize:")
         self.fileText.SetForegroundColour(self.text_colour)                                    
-        self.path = ""
+        self.path = "Enter path.."
         self.chooseFile = wx.Button(self.GetPane(), size=self.itemSize,
                                     label = str('..or select file'))
         self.enterPath = wx.TextCtrl(self.GetPane(), size=self.itemSize,
-                                    value="Enter path..",
+                                    value=self.path,
                                     style=wx.TE_PROCESS_ENTER)
         visSizer.Add(self.fileText, wx.GROW,0)
         visSizer.Add(self.enterPath, wx.GROW,0)
@@ -151,6 +189,7 @@ class VisualizationFrame(wx.CollapsiblePane):
         self.path = openFileDialog.GetPath()
         openFileDialog.Destroy()
         fileFrame.Destroy()
+        self.enterPath.SetValue(self.path)
         print(self.path)
     #When path entered in text and Enter-key is pressed
     def path_OnEnter(self,event):
