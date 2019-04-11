6d0d50f3e7ddbbfc14eb9c368db292f63549650c
Jesper <jjeessppeeer@gmail.com>
2019-04-11 20:57:40 +0200

    Base for slice control panel

--- envision/envision/GUI/frameCharge.py
+++ envision/envision/GUI/frameCharge.py
@@ -37,9 +37,10 @@
 #  <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 import wx, sys, os, h5py
-from parameter_utils import *
+# from parameter_utils import *
 from generalCollapsible import GeneralCollapsible
 from volumeControlCollapsible import VolumeControlCollapsible
+from sliceControlCollapsible import SliceControlCollapsible
 import parameter_utils
 class ChargeFrame(GeneralCollapsible):
     def __init__(self, parent):
@@ -48,6 +49,10 @@ class ChargeFrame(GeneralCollapsible):
         # Setup volume rendering controls
         self.volumeCollapsible = VolumeControlCollapsible(self.GetPane(), "Volume Rendering")
         self.add_sub_collapsible(self.volumeCollapsible)
+
+        #Setup slice controls
+        self.sliceCollapsible = SliceControlCollapsible(self.GetPane(), "Volume Slice")
+        self.add_sub_collapsible(self.sliceCollapsible)
 
         # Override default binding
         # Note that function should be called "on_collapse" to work
