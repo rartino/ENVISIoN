5bc727705e37eff70d16c39e200bb694226885ff
ofaismail <ism@kth.se>
2019-04-11 19:57:13 +0200

    Fixed scaling bug

--- inviwo/modules/graph2d/processors/lineplotprocessor.h
+++ inviwo/modules/graph2d/processors/lineplotprocessor.h
@@ -26,17 +26,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *********************************************************************************/
-/*
- *   Alterations to this file by Andreas Kempe
- *
- *   To the extent possible under law, the person who associated CC0
- *   with the alterations to this file has waived all copyright and
- *   related or neighboring rights to the alterations made to this file.
- *
- *   You should have received a copy of the CC0 legalcode along with
- *   this work.  If not, see
- *   <http://creativecommons.org/publicdomain/zero/1.0/>.
- *********************************************************************************/
  /*
   *   Alterations to this file by Abdullatif Ismail
   *
@@ -123,17 +112,15 @@ class IVW_MODULE_GRAPH2D_API LinePlotProcessor : public Processor {
 public:
     LinePlotProcessor();
     virtual ~LinePlotProcessor() = default;
-
     virtual void process() override;
-
     virtual const ProcessorInfo getProcessorInfo() const override;
     static const ProcessorInfo processorInfo_;
 private:
-    void drawAxes(std::shared_ptr<BasicMesh>& mesh, double x_min, double x_max,
-                  double y_min, double y_max);
-    void drawScale(double x_min, double x_max,
-                   double y_min, double y_max);
-    void drawText(const std::string& text, vec2 position, bool anchor_right = false);
+    void drawAxes(std::shared_ptr<BasicMesh>& mesh, double xMin, double xMax,
+                  double yMin, double yMax);
+    void drawScale(double xMin, double xMax,
+                   double yMin, double yMax);
+    void drawText(const std::string& text, vec2 position, bool anchorRight = false);
     double normalise(double value, double min, double max) const;
 
     DataFrameInport dataFrameInport_;
