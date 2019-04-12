c1ee180d6acce7198167a32593c9e15ece19726d
Linda Le <jjeessppeer@gmail.com>
2019-04-12 19:04:47 +0200

    A fully functional pair-correlation function parser in the case of a different appearance of PCDAT-file (one column, average function for multiple elements).

--- envision/envision/parser/vasp/PKF.py
+++ envision/envision/parser/vasp/PKF.py
@@ -5,7 +5,8 @@ import numpy as np
 
 from .incar import _parse_incar
 from .unitcell import _find_elements
-from ..h5writer import _write_pcdat
+from ..h5writer import _write_pcdat_onecol 
+from ..h5writer import _write_pcdat_multicol
 
 def _parse_pcdat(h5file, vasp_file, vasp_dir):
     #   The function parse PCDAT-file and is called upon by paircorrelation(h5file, vasp_dir)
@@ -38,28 +39,67 @@ def _parse_pcdat(h5file, vasp_file, vasp_dir):
 
       pcdat_data = {}
       total_elements = len(element_list)
-      for i in range(total_elements):
-        pcdat_data[element_list[i]] = []
-        # To make a dictionary of the appearance {'Si':[], 'Au':[]...}
 
+
+      is_pcdatformat_onecol = False 
+
+      if total_elements > 1:       
+        count = 0
+        while count <= 12:
+          count = count + 1 
+          line = vasp_fileobj.readline()
+  
+        vasp_fileobj.seek(0,0) #Set the offset, to begin reading from start again
+              
+        if len(line.split()) == 1: #If there is only one column, average PCF for multiple elements. 
+          is_pcdatformat_onecol = True
+     
+      
+      if is_pcdatformat_onecol: 
+      # In case of multiple elements but only one column in PCDAT-file
+      # create dictinary of appearance {'general paircorr':[]}. 
+        pcdat_data['general paircorr'] = []
         # fill the dictionary pcdat_data
-      count_eof = 0
-      for row_num, content in enumerate(vasp_fileobj):
-        count_eof = row_num
-        
-        if row_num >= 12:
-                 # takes the PKF value columnwise.
-                # The first iteration for example gives
-                # {'Si':[PKF_row1column1], 'Au':[PKF_row1column2], 'K':[PKF_row1column3]...}
-          for column_num in range(total_elements):
-              PKF_value = float(content.split()[column_num])
-                    # dict_key is example 'Si'
-              dict_key = element_list[column_num]
-                    # Fill the pcdat_data dictionary
-              pcdat_data[dict_key].append(PKF_value)
+        count_eof = 0
+        for row_num, content in enumerate(vasp_fileobj):
+          count_eof = row_num
+          if row_num >= 12:
+            PKF_value = float(content.split()[0])
+            pcdat_data['general paircorr'].append(PKF_value)
+ 
+        if count_eof <= 11:
+          raise Exception("PCDAT-file is empty.")
 
-      if count_eof <= 11:
-        raise Exception("PCDAT-file is empty.")
+        return pcdat_data
+          
+      
+      else:     
+        for i in range(total_elements):
+          pcdat_data[element_list[i]] = []
+          # To make a dictionary of the appearance {'Si':[], 'Au':[]...}
+
+          # fill the dictionary pcdat_data
+        count_eof = 0
+
+        for row_num, content in enumerate(vasp_fileobj):
+          count_eof = row_num
+          
+          if row_num >= 12:
+                  # takes the PKF value columnwise.
+                  # The first iteration for example gives
+                  # {'Si':[PKF_row1column1], 'Au':[PKF_row1column2], 'K':[PKF_row1column3]...}
+            for column_num in range(total_elements):
+                PKF_value = float(content.split()[column_num])
+                      # dict_key is example 'Si'
+                dict_key = element_list[column_num]
+                      # Fill the pcdat_data dictionary
+                pcdat_data[dict_key].append(PKF_value)
+
+        if count_eof <= 11:
+          raise Exception("PCDAT-file is empty.")
+        
+        return pcdat_data
+
   return pcdat_data
 
 
@@ -78,15 +118,15 @@ def paircorrelation(h5file, vasp_dir):
     #    ______
     #    Bool: True if parsed, False otherwise.
 
-  vasp_file = os.path.join(vasp_dir, "PCDAT.dms")
+  vasp_file = os.path.join(vasp_dir, "PCDAT")
 
     # See if parsing is already done? If so skip.
   if os.path.isfile(h5file):
       with h5py.File(h5file, "r") as h5:
           if "/PairCorrelationFunc" in h5:
-              print("Already parsed. Skipping.")
-              htest = h5["PairCorrelationFunc/Elements/Cu"]
-              print(htest)
+              print("Already parsed. Skipping.")              
+              htest = h5["PairCorrelationFunc"]
+              print(list(htest.keys()))
               return False
 
     # See if APACO and NPACO is set, otherwise default value is used.
@@ -108,7 +148,12 @@ def paircorrelation(h5file, vasp_dir):
 
   try:
     pcdat_data = _parse_pcdat(h5file, vasp_file, vasp_dir)
-    _write_pcdat(h5file, pcdat_data, APACO_val, NPACO_val)
+    if list(pcdat_data)[0] == "general paircorr": 
+      _write_pcdat_onecol(h5file, pcdat_data, APACO_val, NPACO_val)
+
+    else: 
+      _write_pcdat_multicol(h5file, pcdat_data, APACO_val, NPACO_val)
+
     return True
 
   except FileNotFoundError:
