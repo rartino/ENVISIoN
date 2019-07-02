b3029f4f3fa533b0a9a7220e0efd4d80520a182a
Jesper Ericsson <jjeessppeer@gmail.com>
2019-07-02 15:45:04 +0200

    Started work on python-javascript communication

---
+++ node/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+      width: 1500,
+      height: 1000,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+  
+    // Open the DevTools.
+    win.webContents.openDevTools()
+  
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
+  }
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    // if (process.platform !== 'darwin') {
+    //   app.quit()
+    // }
+    app.quit()
+  })
