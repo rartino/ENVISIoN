17654cef45c93310f9c0e263f6361817860fbf68
Jesper Ericsson <jjeessppeer@gmail.com>
2019-07-02 16:22:05 +0200

    Very simple inviwo instance can be started from electron

--- node/py_js_networking.py
+++ node/py_js_networking.py
@@ -1,7 +1,19 @@
+#  Created by Jesper Ericsson
+# 
+#  To the extent possible under law, the person who associated CC0
+#  with the alterations to this file has waived all copyright and related
+#  or neighboring rights to the alterations made to this file.
+# 
+#  You should have received a copy of the CC0 legalcode along with
+#  this work.  If not, see
+#  <http://creativecommons.org/publicdomain/zero/1.0/>.
 import sys
 import time
 import select
 import json
+
+from ENVISIoN_backend import ENVISIoN
+
 
 # TODO: select method for checking if input is empty will not work on windows.
 
@@ -19,9 +31,11 @@ def process_packet(packet):
     data = decode_packet(packet)
     send_packet("Echo", data)
 
+env = ENVISIoN()
 
 while True:
     time.sleep(1.0/20)
+    env.update()
 
     while select.select([sys.stdin,],[],[],0.0)[0]:
         # print(sys.stdin.readline())
