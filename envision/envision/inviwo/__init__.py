3073ecc0a0fd91f3c3959b17732d97375f8de25d
Jesper Ericsson <jjeessppeer@gmail.com>
2019-07-28 14:35:17 +0200

    removed old files

--- envision/envision/inviwo/__init__.py
+++
@@ -1,7 +0,0 @@
-from .common import *
-# from .unitcell import *
-# from .volume import *
-# from .dos import *
-# from .bandstructure import *
-# from .parchg import *
-# from .PCF import *
--- envision/envision/parser/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import vasp
-from . import elk
