d53f8443c2423963918d154b087f3f1ca4a98356
Jesper Ericsson <jjeessppeer@gmail.com>
2019-07-28 14:35:17 +0200

    removed old files

--- envision/envision/GUI/__init__.py
+++
@@ -1,6 +0,0 @@
-#from .parameter_utils import *
-#from .ENVISIoN import *
-#from .VisFrame import *
-#from .ParserPane import *
-#from .frameCharge import *
-#from .generalCollapsible import *
--- envision/envision/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import parser
-from . import GUI
-from .main import *
