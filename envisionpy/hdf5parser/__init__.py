70e2472d75f205f08a59b6504c6815cc16590fdd
Jesper Ericsson <jjeessppeer@gmail.com>
2019-08-14 15:09:41 +0200

    updated bandstructure inviwo script

--- envisionpy/hdf5parser/__init__.py
+++ envisionpy/hdf5parser/__init__.py
@@ -4,4 +4,5 @@ from .vasp.parchg import parchg
 from .vasp.PCF import paircorrelation
 from .vasp.doscar import dos
 from .vasp.unitcell import unitcell
+from .vasp.fermiEnergy import fermi_energy
 # import envisionpy.hdf5parser.vasp
