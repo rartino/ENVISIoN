72f245bb804862ac260b9363ad0589ad5602e7a4
Jesper Ericsson <jjeessppeer@gmail.com>
2019-08-14 15:00:06 +0200

    updated elf inviwo script

--- scripts/elf.py
+++ scripts/elf.py
@@ -1,7 +1,7 @@
 #
 #  ENVISIoN
 #
-#  Copyright (c) 2018 Viktor Bernholtz
+#  Copyright (c) 2018 Jesper Ericsson
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
@@ -25,47 +25,33 @@
 #  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 ##############################################################################################
-#
-#  Alterations to this file by Viktor Bernholtz, Jesper Ericsson
-#
-#  To the extent possible under law, the person who associated CC0
-#  with the alterations to this file has waived all copyright and related
-#  or neighboring rights to the alterations made to this file.
-#
-#  You should have received a copy of the CC0 legalcode along with
-#  this work.  If not, see
-#  <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+# CONFIGURE FILE PATHS HERE
+
+# Path to your envision installation
+PATH_TO_ENVISION = "C:/Kandidatprojekt/ENVISIoN-sommar"
+
+# Path to the vasp output directory you wish to visualise
+PATH_TO_VASP_CALC = "C:\Kandidatprojekt\VASP\TiPO4_ELF"
+
+# Path to where you want to save the resulting hdf5 file 
+PATH_TO_HDF5 = "C:/Kandidatprojekt/HDF5-demo/elf_demo.hdf5"
 
 import os, sys, inspect, inviwopy
-path_to_current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-sys.path.insert(0, os.path.expanduser(path_to_current_dir + "/../envision"))
+sys.path.append(PATH_TO_ENVISION)
 import envisionpy
-from envisionpy.inviwo.ELFNetworkHandler import ELFNetworkHandler
-
-# Set the path to existing VASP directory and to the desired save location for HDF5-file.
-PATH_TO_VASP_CALC=os.path.expanduser("C:/Kandidatprojekt/VASP/TiPO4_ELF")
-PATH_TO_HDF5=os.path.expanduser("C:/Kandidatprojekt/HDF5/elf_demo.hdf5")
+import envisionpy.hdf5parser
+from envisionpy.processor_network.ELFNetworkHandler import ELFNetworkHandler
 
 # Parse for charge density visualisation.
-envision.parser.vasp.elf(PATH_TO_HDF5, PATH_TO_VASP_CALC)
-try:
-    envision.parser.vasp.unitcell(PATH_TO_HDF5, PATH_TO_VASP_CALC)
-except Exception as error:
-    print(error)
+envisionpy.hdf5parser.elf(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+envisionpy.hdf5parser.unitcell(PATH_TO_HDF5, PATH_TO_VASP_CALC)
 
 # Clear any old network
 inviwopy.app.network.clear()
 
 # Initialize inviwo network
-networkHandler = ELFNetworkHandler(PATH_TO_HDF5)
-
-# Set some default properties, everything can either be 
-# chaged via networkHandler class or directly in
-# the network editor
-
-# Add some default transfer function points
-networkHandler.add_tf_point(0.45, inviwopy.glm.vec4(1, 1, 1, 0))
-networkHandler.add_tf_point(0.5, inviwopy.glm.vec4(0.1, 1, 0.1, 0.1))
+networkHandler = ELFNetworkHandler(PATH_TO_HDF5, inviwopy.app)
 
 # Configure slice visualisation
 networkHandler.toggle_slice_canvas(True)
@@ -75,7 +61,7 @@ networkHandler.set_plane_height(0.5)
 
 # Configure unitcell visualisation
 if networkHandler.unitcellAvailable:
-    networkHandler.hide_atoms(False)
+    #networkHandler.hide_atoms()
     networkHandler.toggle_unitcell_canvas(True)
     networkHandler.set_atom_radius(0.2)
 
