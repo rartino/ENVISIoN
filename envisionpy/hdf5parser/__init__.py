ed05a1c9b9f65e25a6c582ac767c3cf8839cd348
Alexander Vevstad <vevstad@protonmail.com>
2020-05-05 09:53:26 +0200

    fermi parser and new fermi module

--- envisionpy/hdf5parser/__init__.py
+++ envisionpy/hdf5parser/__init__.py
@@ -5,4 +5,5 @@ from .vasp.PCF import paircorrelation
 from .vasp.doscar import dos
 from .vasp.unitcell import unitcell
 from .vasp.fermiEnergy import fermi_energy
+from .vasp.fermi_parser import fermi_parser
 # import envisionpy.hdf5parser.vasp
