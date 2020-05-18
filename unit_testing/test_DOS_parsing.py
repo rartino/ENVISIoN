0c2b4e3513d77e9113424aec46a968ea49cfb0c4
danieldevo <daniel_tomas_96@hotmail.com>
2020-05-18 17:14:14 +0200

    Added unit_testing for more quantities and also added licensing for new files and alteration-licensing for modified files.

---
+++ unit_testing/test_DOS_parsing.py
@@ -0,0 +1,67 @@
+#
+#  ENVISIoN
+#
+#  Copyright (c) 2017-2019 Daniel Thomas
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+##############################################################################################
+
+# Preparements for testing
+
+import os, sys, h5py
+import pytest
+
+# path to current directory
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# check if INVIWO_HOME is set if not assume this unit test is one directory below envision root
+sys.path.append(os.environ.get('INVIWO_HOME', os.path.join(TEST_DIR, os.pardir)))
+import envisionpy.hdf5parser
+
+########################################################################################
+# Test of a VASP-directory which is compatible with the DOS parser.
+
+# Path to the vasp directory
+PATH_TO_VASP_CALC = os.path.join(TEST_DIR, "resources/CuFeS2_band_CBT2")
+
+# Path to the resulting hdf5 file
+PATH_TO_HDF5 = os.path.join(TEST_DIR, "dos_demo.hdf5")
+
+# Parse
+envisionpy.hdf5parser.dos(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+envisionpy.hdf5parser.unitcell(PATH_TO_HDF5, PATH_TO_VASP_CALC)
+
+# Test if the generated HDF5-file contains correct information
+
+def test_parse_1():
+    if os.path.isfile(PATH_TO_HDF5):
+            with h5py.File(PATH_TO_HDF5, 'r') as h5:
+                assert '/DOS' in h5
+                assert '/FermiEnergy' in h5
+                assert '/UnitCell' in h5
+                assert '/basis' in h5
+                assert '/incar' in h5
+                assert '/scaling_factor' in h5
+
+# cleanup
+os.remove(PATH_TO_HDF5)
