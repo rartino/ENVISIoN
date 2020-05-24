113ef2e2ac8c9c5e16014d50f2b82255978d04db
Alexander Vevstad <vevstad@protonmail.com>
2020-05-25 00:38:03 +0200

    updated license

--- envisionpy/hdf5parser/vasp/fermi_parser.py
+++ envisionpy/hdf5parser/vasp/fermi_parser.py
@@ -1,7 +1,7 @@
 #
 #  ENVISIoN
 #
-#  Copyright (c) 2017-2019 Alexander Vevstad
+#  Copyright (c) 2020 Alexander Vevstad
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
