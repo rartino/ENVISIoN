b06ef23473a332d1b83aae0cb3b316491cf536d3
Jesper Ericsson <jjeessppeer@gmail.com>
2020-06-25 10:37:26 +0200

    Visualisation stopping. Elf working

--- envisionpy/network/VisualisationManager.py
+++ envisionpy/network/VisualisationManager.py
@@ -17,17 +17,20 @@ class VisualisationManager():
         self.hdf5Output = self.hdf5Source.getOutport('outport')
 
     def stop(self):
-        # Clear subnetworks and self
-        pass
+        for subnetwork in self.subnetworks:
+            subnetwork.clear_processors()
+        self.network.removeProcessor(self.hdf5Source)
 
     def add_subnetwork(self, network_type):
         if network_type == "charge":
             subnetwork = VolumeSubnetwork(self.app, self.hdf5_path, self.hdf5Output, 0, 3)
             subnetwork.set_hdf5_subpath("/CHG")
-            self.subnetworks.append(subnetwork)
-        if network_type == "atom":
+        elif network_type == "elf":
+            subnetwork = VolumeSubnetwork(self.app, self.hdf5_path, self.hdf5Output, 0, 15)
+            subnetwork.set_hdf5_subpath("/ELF")
+        elif network_type == "atom":
             subnetwork = AtomSubnetwork(self.app, self.hdf5_path, self.hdf5Output, -15, 3)
-            self.subnetworks.append(subnetwork)
+        self.subnetworks.append(subnetwork)
 
             
         
