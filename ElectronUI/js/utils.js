51e1747a23a3e0569729fe4f6a4433c226f1796c
Jesper Ericsson <jjeessppeer@gmail.com>
2020-07-11 15:13:53 +0200

    added input filter function to utils

--- ElectronUI/js/utils.js
+++ ElectronUI/js/utils.js
@@ -54,3 +54,17 @@ function arrayRemoveByValue(arr, value) {
     arr.splice(idx, 1);
     // return arr.filter(item => item !== value);
 }
+
+// Restricts input for each element in the set of matched elements to the given inputFilter.
+$.fn.inputFilter = function (inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+        if (inputFilter(this.value)) {
+            this.oldValue = this.value;
+            this.oldSelectionStart = this.selectionStart;
+            this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+            this.value = this.oldValue;
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+    });
+};
