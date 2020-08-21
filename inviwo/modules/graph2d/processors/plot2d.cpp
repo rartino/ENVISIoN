ae44f768a151002fca6e8ecde272841eeae209e9
Jesper Ericsson <jjeessppeer@gmail.com>
2020-08-21 09:33:26 +0200

    better shader

--- inviwo/modules/graph2d/processors/plot2d.cpp
+++ inviwo/modules/graph2d/processors/plot2d.cpp
@@ -49,7 +49,8 @@ Plot2dProcessor::Plot2dProcessor()
 	, toggle3d_("toggle3d", "Render in 3D space")
 	, lineMesh_( std::make_shared<BasicMesh>() )
 	, meshDrawer_( std::unique_ptr<MeshDrawer>() )
-	, shader_("meshrendering.vert", "meshrendering.frag", false)
+	//, shader_("meshrendering.vert", "meshrendering.frag", false)
+	, shader_("linerenderer.vert", "linerenderer.geom", "linerenderer.frag", false)
 	, lightingProperty_("lighting", "Lighting", &camera_)
     {
 
@@ -115,6 +116,10 @@ Plot2dProcessor::Plot2dProcessor()
 		updatePlotData(); });
 	xAxisSelection_.onChange([this] { updatePlotData(); });
 	yAxisSelection_.onChange([this] { updatePlotData(); });
+	xAxis_.range_.onChange([this] { updatePlotData(); });
+	yAxis_.range_.onChange([this] { updatePlotData(); });
+	xAxis_.useDataRange_.onChange([this] { updatePlotData(); });
+	yAxis_.useDataRange_.onChange([this] { updatePlotData(); });
 	
 	// Initialize axis default values.
 	for (auto axis : { &xAxis_, &yAxis_ }) {
@@ -137,8 +142,25 @@ Plot2dProcessor::Plot2dProcessor()
 
 void Plot2dProcessor::initializeResources() {
 	LogInfo("Initializing resources plot2d");
-	utilgl::addShaderDefines(shader_, lightingProperty_);
+
+	shader_[ShaderType::Geometry]->addShaderDefine("ENABLE_ADJACENCY", "1");
+	//shader_[ShaderType::Fragment]->addShaderDefine("ENABLE_ROUND_DEPTH_PROFILE");
+
+	// See createLineStripMesh()
+	shader_[ShaderType::Vertex]->addInDeclaration("in_" + toString(BufferType::PositionAttrib),
+		static_cast<int>(BufferType::PositionAttrib),
+		"vec3");
+	shader_[ShaderType::Vertex]->addInDeclaration("in_" + toString(BufferType::ColorAttrib),
+		static_cast<int>(BufferType::ColorAttrib),
+		"vec4");
+	shader_[ShaderType::Vertex]->addInDeclaration("in_" + toString(BufferType::TexcoordAttrib),
+		static_cast<int>(BufferType::TexcoordAttrib),
+		"vec2");
 	shader_.build();
+	shader_.activate();
+	shader_.setUniform("antialiasing", 0.8f);
+	shader_.setUniform("miterLimit", 1.0f);
+	shader_.deactivate();
 }
 
 
@@ -160,76 +182,18 @@ void Plot2dProcessor::process() {
 	else
 		activeCamera = camera2d_.get();
 
-
-	auto dot = [](const vec3& v1, const vec3& v2) { return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;  };
-	auto cross = [](const vec3& v1, const vec3& v2) { return vec3(
-		v1.y * v2.z - v1.z * v2.y,
-		v1.z * v2.x - v1.x * v2.z,
-		v1.x * v2.y - v1.y * v2.x); };
-	auto length = [](const vec3& v) { return std::sqrt(v.x*v.x + v.y*v.y + v.z*v.z); };
-	auto normalize = [length](const vec3& v) { return v / length(v); };
-
 	vec3 xDir(1, 0, 0);
 	vec3 yDir(0, 1, 0);
 
 	if (toggle3d_.get()) {
 		// Direct the graph to face the camera.
-		auto from = camera_.getLookFrom();
-		auto to = camera_.getLookTo();
-		auto up = camera_.getLookUp();
-		//glm::dot(from, to);
-
-		auto normal = from - to;
-		normal = normalize(normal);
-		xDir = vec3(normal[1], -normal[0], 0);
-		auto a = normal * xDir;
-		yDir = cross(normal, xDir);
-
-		xDir = normalize(xDir);
-		yDir = normalize(yDir);
-
-		xDir = xDir * dot(xDir, up) + yDir * dot(yDir, up);
-		xDir = normalize(xDir);
-
-		yDir = cross(normal, xDir);
-		yDir = -normalize(yDir);
-		
-
-		std::swap(yDir, xDir);
-
-		LogInfo("Camera: \n"
-			<< from[0] << ", " << from[1] << ", " << from[2] << "\n"
-			<< to[0] << ", " << to[1] << ", " << to[2] << "\n"
-			<< up[0] << ", " << up[1] << ", " << up[2] << "\n"
-		);
-		//camera_.setLookFrom(vec3(0, 0, from[2]));
-		//camera_.setLookUp(vec3(0, 1, 0));
-		/*lineMesh_->setBasis(Matrix<3U, float>(
-			graphDims_[0], 0, 0, 
-			0, graphDims_[1], 0, 
-			0, 0, 1));*/
-		/*lineMesh_->setBasis(Matrix<3U, float>(
-			graphDims_[0]*xDir.x, yDir.x, 0,
-			xDir.y, graphDims_[1]*yDir.y, 0,
-			xDir.z, yDir.z, 1));*/
-		/*lineMesh_->setBasis(Matrix<3U, float>(
-			graphDims_[0] * xDir.x, xDir.y, xDir.z,
-			yDir.x, graphDims_[1] * yDir.y, yDir.z,
-			0, 0, 1));*/
+		yDir = glm::normalize( camera_.getLookUp() );
+		xDir = glm::normalize( glm::cross(yDir, camera_.getLookFrom() - camera_.getLookTo()) );
 
 		lineMesh_->setBasis(Matrix<3U, float>(
 			graphDims_[0] * xDir.x, graphDims_[0] * xDir.y, graphDims_[0] * xDir.z,
 			graphDims_[1] * yDir.x, graphDims_[1] * yDir.y, graphDims_[1] * yDir.z,
 			0, 0, 1));
-
-		//lineMesh_->setOffset(origin_);
-
-		LogInfo("Normal: \n"
-			<< length(normal) << ", " << length(xDir) << ", " << length(yDir) << "\n"
-			<< dot(normal, xDir) << ", " << dot(normal, yDir) << ", " << dot(xDir, yDir) << "\n"
-			<< normal[0] << ", " << normal[1] << ", " << normal[2] << "\n"
-		);
-		//camera_.setLookFrom(vec3(from[0], 0, 0));
 	}
 
 	// Render x and y axis
@@ -242,9 +206,17 @@ void Plot2dProcessor::process() {
 	
 	// Render lines.
 	shader_.activate();
-	utilgl::setUniforms(shader_, lightingProperty_);
-	utilgl::setShaderUniforms(shader_, *activeCamera, "camera");
+	//utilgl::setUniforms(shader_, lightingProperty_);
+	
+	
 	utilgl::setShaderUniforms(shader_, *meshDrawer_->getMesh(), "geometry");
+	utilgl::setShaderUniforms(shader_, *activeCamera, "camera");
+	shader_.setUniform("screenDim", vec2(outport_.getDimensions()));
+	//shader_.setUniform("camera", *activeCamera);
+	shader_.setUniform("lineWidth", 2);
+
+
+
 	meshDrawer_->draw();
 	shader_.deactivate();
 
@@ -298,13 +270,6 @@ void Plot2dProcessor::updateDimensions() {
 
 
 void Plot2dProcessor::updatePlotData() {
-	// Clear index buffers
-	//lineMesh_->clearRepresentations();
-	/*while (lineMesh_->getIndexBuffers().size() != 0){
-		lineMesh_->getIndexBuffers()[0].second->clearRepresentations();
-		lineMesh_->removeIndexBuffer(0);
-	}*/
-
 	// Reset line mesh
 	lineMesh_ = std::make_shared<BasicMesh>(); 
 	meshDrawer_ = std::move(InviwoApplication::getPtr()->getMeshDrawerFactory()->create(lineMesh_.get()));
@@ -316,38 +281,67 @@ void Plot2dProcessor::updatePlotData() {
 	const std::shared_ptr<const Column> xCol = dataframe->getColumn(xAxisSelection_.getSelectedIndex());
 	const std::shared_ptr<const Column> yCol = dataframe->getColumn(yAxisSelection_.getSelectedIndex());
 
+
+	size_t nValues = xCol->getSize();
+
 	if (xCol->getSize() < 2 || yCol->getSize() < 2 || yCol->getSize() != xCol->getSize()) return;
 	
-	// Find extreme values for x and y axis
-	dvec2 xrange(xCol->getAsDouble(0), xCol->getAsDouble(0));
-	dvec2 yrange(yCol->getAsDouble(0), yCol->getAsDouble(0));
-	for (size_t i = 1; i < xCol->getSize(); ++i) {
-		double v = xCol->getAsDouble(i);
-		if (v < xrange[0]) xrange[0] = v;
-		if (v > xrange[1]) xrange[1] = v;
+	
+
+
+	// Sort by x axis?
+
+
+	// Set ranges for x and y axis
+	if (xAxis_.getUseDataRange()) {
+		LogInfo("Autosetting x range");
+		dvec2 xrange(xCol->getAsDouble(0), xCol->getAsDouble(0));
+		for (size_t i = 1; i < nValues; ++i) {
+			double v = xCol->getAsDouble(i);
+			if (v < xrange[0]) xrange[0] = v;
+			if (v > xrange[1]) xrange[1] = v;
+		}
+		xAxis_.range_.set(xrange);
 	}
-	for (size_t i = 1; i < yCol->getSize(); ++i) {
-		double v = yCol->getAsDouble(i);
-		if (v < yrange[0]) yrange[0] = v;
-		if (v > yrange[1]) yrange[1] = v;
+
+	if (yAxis_.getUseDataRange()) {
+		LogInfo("Autosetting y range");
+		dvec2 yrange(yCol->getAsDouble(0), yCol->getAsDouble(0));
+		for (size_t i = 1; i < nValues; ++i) {
+			if (xCol->getAsDouble(i) < xAxis_.getRange()[0] || xCol->getAsDouble(i) > xAxis_.getRange()[1]) {
+				LogInfo("Bad X: " << xCol->getAsDouble(i));
+				continue;
+			}
+			double v = yCol->getAsDouble(i);
+			if (v < yrange[0]) yrange[0] = v;
+			if (v > yrange[1]) yrange[1] = v;
+		}
+		if (yrange[0] == yrange[1]) yrange[1] += 1;
+		yAxis_.range_.set(yrange);
 	}
-	xAxis_.range_.set(xrange);
-	yAxis_.range_.set(yrange);
 
-	const float offsetX = -xrange[0];
-	const float offsetY = -yrange[0];
-	const float scaleX = 1.0f / (xrange[1] - xrange[0]);
-	const float scaleY = 1.0f / (yrange[1] - yrange[0]);
+	const double offsetX = -xAxis_.getRange()[0];
+	const double offsetY = -yAxis_.getRange()[0];
+	const double scaleX = 1.0f / (xAxis_.getRange()[1] - xAxis_.getRange()[0]);
+	const double scaleY = 1.0f / (yAxis_.getRange()[1] - yAxis_.getRange()[0]);
 
-	std::shared_ptr<IndexBufferRAM> indices = lineMesh_->addIndexBuffer(DrawType::Lines, ConnectivityType::Strip);
-
+	//std::shared_ptr<IndexBufferRAM> indices = lineMesh_->addIndexBuffer(DrawType::Lines, ConnectivityType::None);
+	std::shared_ptr<IndexBufferRAM> indices = lineMesh_->addIndexBuffer(DrawType::Lines, ConnectivityType::StripAdjacency);
 	vec4 color(1, 0, 0, 1);
+	const auto notInRange = [](const vec3& p) { 
+		return p.x < 0 || p.x > 1 || p.y < 0 || p.y > 1;
+	};
+
+	auto getPoint = [xCol, yCol, offsetX, offsetY, scaleX, scaleY](const size_t index) {
+		return vec3((xCol->getAsDouble(index) + offsetX)*scaleX, (yCol->getAsDouble(index) + offsetY)*scaleY, 0);
+	};
 	//// Add point from dataframe to line mesh
-	for (size_t i = 0; i < xCol->getSize(); i+=1) {
+	for (size_t i = 0; i < nValues; ++i) {
 		const vec3 p1((xCol->getAsDouble(i) + offsetX)*scaleX, (yCol->getAsDouble(i) + offsetY)*scaleY, 0);
 		//const vec3 p2((xCol->getAsDouble(i+1) + offsetX)*scaleX, (yCol->getAsDouble(i+1) + offsetY)*scaleY, 0);
-		
+		if (notInRange(p1)) continue;
 		indices->add(lineMesh_->addVertex(p1, p1, p1, color));
+		//indices->add(lineMesh_->addVertex(p2, p2, p2, color));
 	}
 	
 
