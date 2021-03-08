572b7b95395b60dae2567de3f4c50b2879b17fff
Adam Engman <adamengman1@gmail.com>
2021-03-08 12:33:50 +0100

    Integrerat force parse med Inviwo

--- envisionpy/hdf5parser/__init__.py
+++ envisionpy/hdf5parser/__init__.py
@@ -6,4 +6,5 @@ from .vasp.doscar import dos
 from .vasp.unitcell import unitcell
 from .vasp.fermiEnergy import fermi_energy
 from .vasp.fermi_parser import fermi_parser
+from .vasp.force_parser import force_parser
 # import envisionpy.hdf5parser.vasp
