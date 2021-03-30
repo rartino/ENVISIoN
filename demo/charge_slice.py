84707c2fa69f7d8855b0689e92e1ca4ed14add35
Adam Engman <adamengman1@gmail.com>
2021-03-30 19:20:56 +0200

    Ritar ut endast atomer för alla givna VASP

--- demo/charge_slice.py
+++ demo/charge_slice.py
@@ -7,11 +7,11 @@ import envisionpy.hdf5parser
 from envisionpy.network import VisualisationManager
 
 # Path to the vasp output directory you wish to visualise
-VASP_DIR = path_to_current_folder + "/../unit_testing/resources/NaCl_charge_density"
-HDF5_FILE = path_to_current_folder + "/../unit_testing/resources/nacl18.hdf5"
+VASP_DIR = path_to_current_folder + "/../unit_testing/resources/Cu_band_CUB"
+HDF5_FILE = path_to_current_folder + "/../unit_testing/resources/test.hdf5"
 
 # Parse for charge density visualisation.
-envisionpy.hdf5parser.charge(HDF5_FILE, VASP_DIR)
+#envisionpy.hdf5parser.charge(HDF5_FILE, VASP_DIR)
 envisionpy.hdf5parser.unitcell(HDF5_FILE, VASP_DIR)
 
 # Clear any old network
@@ -20,10 +20,10 @@ inviwopy.app.network.clear()
 # Initialize inviwo network
 visManager = VisualisationManager(HDF5_FILE, inviwopy.app)
 
-visManager.start("charge")
+#visManager.start("charge")
 visManager.start("atom")
 
-visManager.subnetworks['charge'].toggle_slice_plane(True)
-visManager.subnetworks['charge'].show(False, True)
+#visManager.subnetworks['charge'].toggle_slice_plane(True)
+#visManager.subnetworks['charge'].show(False, True)
 
 
