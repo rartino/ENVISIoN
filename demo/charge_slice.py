568c878da4e0177afa48e169baf26782a6e22c27
ofaismail <ism@kth.se>
2021-04-08 10:26:18 +0200

    Gjort sista ändringar för CoordnateReader och sparat workspace för BP4-möte

--- demo/charge_slice.py
+++ demo/charge_slice.py
@@ -8,10 +8,10 @@ from envisionpy.network import VisualisationManager
 
 # Path to the vasp output directory you wish to visualise
 VASP_DIR = path_to_current_folder + "/../unit_testing/resources/Cu_band_CUB"
-HDF5_FILE = path_to_current_folder + "/../unit_testing/resources/test.hdf5"
+HDF5_FILE = path_to_current_folder + "/../unit_testing/resources/DemoSlice.hdf5"
 
 # Parse for charge density visualisation.
-#envisionpy.hdf5parser.charge(HDF5_FILE, VASP_DIR)
+envisionpy.hdf5parser.charge(HDF5_FILE, VASP_DIR)
 envisionpy.hdf5parser.unitcell(HDF5_FILE, VASP_DIR)
 
 # Clear any old network
@@ -20,10 +20,10 @@ inviwopy.app.network.clear()
 # Initialize inviwo network
 visManager = VisualisationManager(HDF5_FILE, inviwopy.app)
 
-#visManager.start("charge")
+visManager.start("charge")
 visManager.start("atom")
 
-#visManager.subnetworks['charge'].toggle_slice_plane(True)
-#visManager.subnetworks['charge'].show(False, True)
+visManager.subnetworks['charge'].toggle_slice_plane(True)
+visManager.subnetworks['charge'].show(False, True)
 
 
