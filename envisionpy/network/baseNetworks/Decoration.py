5b19ba69975889b26ab2912d6b1c201839feebe5
Gabriel Anderberg <gaban592@student.liu.se>
2021-04-13 16:50:35 +0200

    Börjat skissa på utritning av kraftvektorpilar i Envision GUI

--- envisionpy/network/baseNetworks/Decoration.py
+++ envisionpy/network/baseNetworks/Decoration.py
@@ -11,6 +11,7 @@ class Decoration(Subnetwork):
     def __init__(self, inviwoApp):
         Subnetwork.__init__(self, inviwoApp)
         self.other_subnetworks = {}
+        #self.force_enabled = False
 
     def __del__(self):
         # Disconnect this from other networks.
@@ -27,13 +28,26 @@ class Decoration(Subnetwork):
         if vis_type not in self.valid_visualisations():
             raise EnvisionError('Invalid visualisation type ['+vis_type+'].')
         self.other_subnetworks[vis_type] = other
-    
+
     # Should be overloaded in inheritor class
     def disconnect_decoration(self, other, vis_type):
         pass
-        
 
-    
-
-    
-
+    #Skissat från toggle_iso i VolumeSubnetwork.py rad 164
+    # def toggle_force(self, enable)
+    #     #Kolla om vi vill visa kraftvektorpilar
+    #     self.force_enabled = enable
+    #     #Kolla om nåt villkor (is_multichannel) är uppfyllt
+    #     if self.is_multichannel:
+    #         if enable:
+    #             #Rita ut kraftvektorpilar
+    #             set_force_vectors
+    #         else:
+    #             #Avaktivera utritning av kraftvektorpilar
+    #             sätt inte ut kraftvektorpilar
+    #     else:
+    #         osäker på vad vi ska göra här
+    #
+    # def set_force_vectors(self, value, color=None):
+    #     Skriv den här funktionen som ska rita ut alternativt
+    #     ta bort kraftvektorpilar från visualiseringen
