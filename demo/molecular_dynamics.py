d894b8370a21e99c17e4864eda7e956b6b16ecff
Kristoffer Gubberud Maras <krima334@student.liu.se>
2021-04-16 07:35:14 -0700

    Fortsätter testa nätverket. Felsöker och fixar så att XDATCAR-filen kan köras genom nätverket.

--- demo/molecular_dynamics.py
+++ demo/molecular_dynamics.py
@@ -7,10 +7,13 @@ import envisionpy.hdf5parser
 from envisionpy.network import VisualisationManager
 
 VASP_DIR = path_to_current_folder + "/../unit_testing/resources/Cu_band_CUB"
-HDF5_FILE = path_to_current_folder + "/../demo_molecular_dynamics.hdf5"
+#HDF5_FILE = path_to_current_folder + "/../demo_molecular_dynamics.hdf5"
+
+#Temporär testning med färdig-genererad HDF5-fil
+HDF5_FILE = path_to_current_folder + "/../md_test.hdf5"
 
 #parse for molecular dynamics
-envisionpy.hdf5parser.mol_dynamic_parser(HDF5_FILE, VASP_DIR)
+#envisionpy.hdf5parser.mol_dynamic_parser(HDF5_FILE, VASP_DIR)
 
 #clear any old network
 inviwopy.app.network.clear()
