437cfef8d80140e53b3551aec5d4ff312bd907aa
Gabriel <gaban592@student.liu.se>
2021-04-23 09:50:10 +0200

    små fix

--- envisionpy/hdf5parser/vasp/force_parser.py
+++ envisionpy/hdf5parser/vasp/force_parser.py
@@ -88,6 +88,7 @@ def _parse_forces(vasp_dir, get_vector_tips = False, coordinates = []):
         for n, line in enumerate(lines):
             if 'total drift' in line:
                 pos = n - 2
+                break
         for i, line in enumerate(lines):
             if 'POSITION' in line:
                 k = i + 1
@@ -96,6 +97,7 @@ def _parse_forces(vasp_dir, get_vector_tips = False, coordinates = []):
                     force = [float(x) for x in force]
                     forces.append(force)
                     k += 1
+                break
     if get_vector_tips:
         for i in range(len(forces)):
             list = [x + y for x, y in zip(coordinates[i], forces[i])]
