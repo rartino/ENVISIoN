cd4e8b04f9b2061528d64c5d871d4c02b67f6453
Adam Engman <adamengman1@gmail.com>
2021-05-03 10:41:28 +0200

    Added example directory checker for force_parser

--- envisionpy/hdf5parser/__init__.py
+++ envisionpy/hdf5parser/__init__.py
@@ -6,10 +6,11 @@ from .vasp.doscar import dos
 from .vasp.unitcell import unitcell
 from .vasp.fermiEnergy import fermi_energy
 from .vasp.fermi_parser import fermi_parser
-from .vasp.force_parser import force_parser
+from .vasp.force_parser import force_parser, check_directory_force_parser
 from .vasp.molecular_dynamics_parser import mol_dynamic_parser
 from .vasp.check_for_parse import has_been_parsed
 from .ELK.unitcell_parser import unitcell_parser
 from .vasp.bandstructure_combo import bandstructure_combo
 from .vasp.bandstructure_combo import bandstructure_combo3d
+
 # import envisionpy.hdf5parser.vasp
