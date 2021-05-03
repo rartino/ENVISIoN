caa75baee86abb237ff1e5c810beddfa7ab4fff0
Adam Engman <adamengman1@gmail.com>
2021-05-03 11:16:03 +0200

    Added function type blocks to GUI

--- envisionGUI/GUI.py
+++ envisionGUI/GUI.py
@@ -24,6 +24,11 @@ def send_request(rtype, data):
 envisionMain = EnvisionMain()
 
 sg.theme('DarkGrey14')
+
+# ------------------------------------------------------------------------- #
+#                         Variables and Globals                             #
+# ------------------------------------------------------------------------- #
+
 current_dataset = None
 current_folder = None
 max_datasets = 5
@@ -39,6 +44,10 @@ visualisations = {'Force' : 'force',
                   'ELF' : 'elf',
                   'Fermi Surface' : 'fermi',
                   'Atom Positions' : 'atom'}
+
+# ------------------------------------------------------------------------- #
+#                       Functions that set up the GUI                       #
+# ------------------------------------------------------------------------- #
 
 def setup_datasets(return_keys = False):
     global dataset_dir, dataset_vises
@@ -134,19 +143,229 @@ def set_selected_folder(values):
 def get_loaded_datasets():
     return tuple([i for i in dataset_dir.values() if i != None])
 
-def parse(vasp_path = None):
-    num = rd.randint(0,100)
-    if num < 50:
-        vises = ['Force' , 'BandStructure', 'Molecular Dynamics']
+
+# ------------------------------------------------------------------------- #
+#                        Parsing related functions                          #
+# ------------------------------------------------------------------------- #
+
+def parse(vasp_path, current_dataset):
+    clear_hdf5(current_dataset)
+    pos_vises = []
+    if envisionpy.hdf5parser.check_directory_force_parser(vasp_path):
+        pos_vises.append('Force')
+        envisionpy.hdf5parser.force_parser('force' + current_dataset + '.hdf5', vasp_path)
+    # Följt av if satser för alla parsers.
+    set_dataset_to_vises_and_dir(vasp_path, pos_vises)
+
+def clear_hdf5(current_dataset):
+    try:
+        test = os.listdir(path_to_current_folder + '/../envisionGUI/')
+        print(test)
+        for item in test:
+            if item.endswith(current_dataset + ".hdf5"):
+                os.remove(os.path.join(path_to_current_folder + '/../envisionGUI',item))
+        return True
+    except:
+        print('Couldnt remove')
+
+
+# ------------------------------------------------------------------------- #
+#            Functions that control the look of visualisations              #
+# ------------------------------------------------------------------------- #
+
+def toggle_canvas(file, type):
+    global canvas
+    #try:
+    if canvas:
+        envisionMain.update()
+        send_request('visualisation_request', [file, type, "hide", []])
+        envisionMain.update()
+        canvas = False
     else:
-        vises =  ['PCF', 'ELF', 'Charge']
-    set_dataset_to_vises_and_dir(vasp_path, vises)
+        envisionMain.update()
+        send_request('visualisation_request', [file, type, "show", []])
+        envisionMain.update()
+        canvas = True
+        #console_message('Canvas Toggled')
+    #except:
+        #console_message('Failed to toggle Canvas')
+    return
+
+def set_shading_mode(file, type, key):
+    try:
+       send_request("visualisation_request",
+                    [file, type, "set_shading_mode", [key]])
+    except:
+       console_message('Could not set shading mode')
+
+def set_color(file, type, key):
+    try:
+       send_request("visualisation_request",
+                    [file, type, "set_color", [colors[key]]])
+    except:
+       console_message('Could not set color')
+
+def set_volume_selection(file, type, key):
+    try:
+       send_request("visualisation_request", [file, type,
+                    "set_volume_selection", [key]])
+    except:
+       console_message('Could not set volume selection')
+
+def toggle_force_vectors(file, type):
+    global vectors
+    try:
+        if vectors:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type, "hide_vectors"])
+            envisionMain.update()
+            vectors = False
+        else:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type, "show_vectors"])
+            envisionMain.update()
+            vectors = True
+        console_message('Vectors Toggled')
+    except:
+        console_message('Failed to toggle Vectors')
+    return
+
+def toggle_iso_surface(file, type):
+    global toggle_iso
+    try:
+        if toggle_iso:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type, 'toggle_iso',
+                         [toggle_iso]])
+            envisionMain.update()
+            toggle_iso = False
+        else:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type, 'toggle_iso',
+                         [toggle_iso]])
+            envisionMain.update()
+            toggle_iso = True
+        console_message('ISO Toggled')
+    except:
+        console_message('Failed to toggle ISO')
+    return
+
+def set_iso_surface(file, type, value):
+    try:
+        send_request("visualisation_request", [file, type, "set_iso_surface",
+                     [value]])
+    except:
+        console_message('Could not set ISO-surface value')
+
+def set_radius(file, type, value):
+    try:
+        send_request("visualisation_request", [file, type, "set_radius",
+                     [value]])
+    except:
+        console_message('Could not set radius')
+
+def toggle_slice_canvas(file, type):
+    global slice_canvas
+    try:
+        if slice_canvas:
+            envisionMain.update()
+            send_request('visualisation_request', [file, type, "hide",
+                         [False, True]])
+            envisionMain.update()
+            slice_canvas = False
+        else:
+            envisionMain.update()
+            send_request('visualisation_request', [file, type, "show",
+                         [False, True]])
+            envisionMain.update()
+            slice_canvas = True
+        console_message('Slice Canvas Toggled')
+    except:
+        console_message('Failed to toggle Slice Canvas')
+    return
+
+def toggle_slice_plane(file, type):
+    global slice_plane
+    try:
+        if slice_plane:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type,
+                         'toggle_slice_plane', [slice_plane]])
+            envisionMain.update()
+            slice_plane = False
+        else:
+            envisionMain.update()
+            send_request("visualisation_request", [file, type,
+                         'toggle_slice_plane', [slice_plane]])
+            envisionMain.update()
+            slice_plane = True
+        console_message('Slice Plane Toggled')
+    except:
+        console_message('Failed to toggle Slice Plane')
+    return
+
+def set_slice_plane_height(file, type, value):
+    try:
+        send_request("visualisation_request", [file, type,
+                     "set_plane_height", [value]])
+    except:
+        console_message('Could not set slice plane height')
+
+def unfinished(file, type):
+    console_message('Not yet implemented')
+    return
+
+def set_animation_speed(file, type, speed):
+    speed = round(-3.7647 + 103.7647*2.7182**(-3.3164*speed))
+    try:
+        send_request("visualisation_request", [file, type, "set_speed",
+                     [speed]])
+    except:
+        console_message('Could not set speed')
+
+def set_opacity(file, type, value):
+    try:
+        send_request("visualisation_request", [file, type, "set_opacity",
+                     [value]])
+    except:
+        console_message('Could not set opacity')
+
+def play_pause(file, type):
+    try:
+        send_request("visualisation_request", [file, type, "play_pause"])
+    except:
+        console_message('Could not set play/pause')
+
+def set_standard_parameters(file, type):
+    try:
+        set_radius(file, type, 0.5)
+    except:
+        pass
+    try:
+        set_animation_speed(file, type, 0.5)
+    except:
+        pass
+    try:
+        set_opacity(file, type, 1)
+    except:
+        pass
+
+# ------------------------------------------------------------------------- #
+#                             Layout Settings                               #
+# ------------------------------------------------------------------------- #
 
 layout = [[sg.Frame(layout = setup_datasets(), title = ''),
-           sg.Frame(layout = setup_folderloader(), title = '', vertical_alignment = 'bottom')],
-           [sg.Frame(layout = setup_vis_buttons(), title = '')]]
+           sg.Frame(layout = setup_folderloader(), title = '',
+           vertical_alignment = 'bottom')],
+          [sg.Frame(layout = setup_vis_buttons(), title = '')]]
+
 
 window = sg.Window('',layout)
+
+# ------------------------------------------------------------------------- #
+#                               Event Loop                                  #
+# ------------------------------------------------------------------------- #
+
 while True:
     envisionMain.update()
     event, values = window.read(timeout = 10)
@@ -156,7 +375,7 @@ while True:
     if (event == 'parsefolder' and current_folder != None
                                and current_folder not in get_loaded_datasets()
                                and current_dataset != None):
-        parse(current_folder)
+        parse(current_folder, current_dataset)
     if event in setup_datasets(True):
         set_selected_dataset(event)
         switch_dataset(event)
