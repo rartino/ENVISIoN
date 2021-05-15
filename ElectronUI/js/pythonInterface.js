ad31c3fa76022e696dcd55fe75c7b294d751c8bd
ofaismail <ism@kth.se>
2021-05-15 13:13:19 +0200

    Added and updated more BSD 2-Clause to files

--- ElectronUI/js/pythonInterface.js
+++ ElectronUI/js/pythonInterface.js
@@ -1,6 +1,6 @@
 //  ENVISIoN
 //
-//  Copyright (c) 2019-2021 Jesper EricssonGabriel Anderberg, Didrik Axén,  Adam Engman,
+//  Copyright (c) 2019-2021 Jesper Ericsson, Gabriel Anderberg, Didrik Axén, Adam Engman,
 //  Kristoffer Gubberud Maras, Joakim Stenborg
 //  All rights reserved.
 //
@@ -25,6 +25,16 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // ##############################################################################################
+//  Alterations to this file by Gabriel Anderberg, Didrik Axén,
+//  Adam Engman, Kristoffer Gubberud Maras, Joakim Stenborg
+//
+//  To the extent possible under law, the person who associated CC0 with
+//  the alterations to this file has waived all copyright and related
+//  or neighboring rights to the alterations made to this file.
+//
+//  You should have received a copy of the CC0 legalcode along with
+//  this work.  If not, see
+//  <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 const spawn = require("child_process").spawn;
 const CONFIG = require("./config.json");
